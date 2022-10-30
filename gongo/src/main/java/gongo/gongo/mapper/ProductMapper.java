512fe4e7d2c7cc65ae1620251740ba1182491c8e
LukeEom <eluke1028@gmail.com>
2022-10-30 23:06:00 +0900

    Spring Security retry

--- gongo/src/main/java/gongo/gongo/mapper/ProductMapper.java
+++ gongo/src/main/java/gongo/gongo/mapper/ProductMapper.java
@@ -3,14 +3,12 @@ package gongo.gongo.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-import gongo.gongo.dto.ProductDTO;
-import gongo.gongo.entity.Product;
+
 
 @Mapper
 public  interface ProductMapper{
     
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
-    @Override
-    ProductDTO toDto(Product entity);
+    
 }
