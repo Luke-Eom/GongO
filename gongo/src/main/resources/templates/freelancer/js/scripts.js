83b4c37f87ecafa0c28f7aa38acb7f21760f4502
LukeEom <eluke1028@gmail.com>
2022-11-02 15:58:18 +0900

    index update

--- gongo/src/main/resources/templates/freelancer/js/scripts.js
+++ gongo/src/main/resources/templates/freelancer/js/scripts.js
@@ -66,4 +66,21 @@
     });
   
   })(jQuery); // End of use strict
+
+
+function openCatTab(evt, productCategory) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(productCategory).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();  
   
